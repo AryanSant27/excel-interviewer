e57728f54715168ec1bd13adba439d084401254a
Aryan Sant <santaryan27@gmail.com>
2025-07-31 19:11:54 +0530

    CORS 2

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://excel-interviewer.vercel.app"}})
+CORS(
+    app,
+    origins=["https://excel-interviewer.vercel.app"],
+    methods=["GET", "POST", "OPTIONS"],
+    supports_credentials=True,
+    allow_headers=["Content-Type"]
+)
 
 from app import routes
